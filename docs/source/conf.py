649eb02b15e4160bed9fa0bead83104b70371359
huangyuxin <hyxin2014@126.com>
2021-10-15 05:12:14 +0000

    try

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,3 +33,21 @@ html_theme = 'sphinx_rtd_theme'
 
 # -- Options for EPUB output
 epub_show_urls = 'footnote'
+
+
+from recommonmark.parser import CommonMarkParser
+source_parsers = {
+            '.md': CommonMarkParser,
+            }
+source_suffix = ['.rst', '.md']
+
+import recommonmark
+from recommonmark.transform import AutoStructify
+
+# At the bottom of conf.py
+def setup(app):
+    app.add_config_value('recommonmark_config', {
+                        'url_resolver': lambda url: github_doc_root + url,
+                                    'auto_toc_tree_section': 'Contents',
+                                                }, True)
+    app.add_transform(AutoStructify)
